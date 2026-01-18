bb1f6fa4504ec9ecc2dbaff29fe2077169471829
dexm0rggan-crypto <dexm0rggan@gmail.com>
2026-01-18 13:37:27 -0500

    Comint

--- Semana6/obj/Debug/net8.0/Semana6.AssemblyInfo.cs
+++ Semana6/obj/Debug/net8.0/Semana6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719296b22ddfb6bde95d8958b720252c97209b4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937158a7748e727e4768534f91a4465d7037c361")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
